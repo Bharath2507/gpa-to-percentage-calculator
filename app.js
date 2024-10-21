887de1199c957295bf0cd432a32d040a1cc70ea5
Bharath2507 <36845326+Bharath2507@users.noreply.github.com>
2024-10-21 10:49:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 function convertToPercentage() {
     let gpa = parseFloat(document.getElementById("gpa").value);
     let percentage;
-    
     if (gpa >= 4.00 && gpa < 5.00) {
         percentage = ((gpa - 4.00) * 10) + 50;
     } else if (gpa >= 5.00 && gpa < 7.50) {
         percentage = ((gpa - 5.00) * 8) + 60;
     } else if (gpa >= 7.50 && gpa <= 10.00) {
         percentage = ((gpa - 7.50) * 7) + 80;
-    } else if (gpa == 7.71) {
-        percentage = "65 for GPRS and 81.47 for others."
     } else {
         percentage = "Invalid CGPA input. Please enter a CGPA between 4.00 and 10.00.";
     }
-
     document.getElementById("result").innerHTML = `Your Percentage: ${percentage.toFixed(2)}%`;
 }
